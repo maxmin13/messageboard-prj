518e71cb8a0df242a700eb6f3a59d0af72622d11
vagrant <vagrant@maxmin.com>
2023-09-26 15:48:45 +0100

    project settings fileus

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
 
     path("admin/", admin.site.urls),
-    path("", include("apps.posts.urls")),  
+    path("posts", include("apps.posts.urls")),  
 
     
 ]
