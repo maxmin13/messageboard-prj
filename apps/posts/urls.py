50283e0863dcb1938cee67a7b09d09ee14a6dfc6
vagrant <vagrant@maxmin.com>
2023-10-07 11:08:48 +0100

    Django first app

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from apps.posts.views import PostsView
 
 urlpatterns = [
-   path("posts/", PostsView.as_view(), name="posts"),
+   path(r"^posts/", PostsView.as_view(), name="posts"),
     
 ]
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,4 +1,3 @@
-# pages/views.py
 from django.views.generic import TemplateView
 from django.views.generic import ListView
 from .models import Post
