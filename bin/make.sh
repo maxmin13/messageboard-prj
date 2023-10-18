a70c90cdaa93c29321b11cb64cbd630c2a0195c4
vagrant <vagrant@maxmin.com>
2023-10-18 17:23:03 +0100

    configuration refactoring

--- bin/make.sh
+++ bin/make.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # shellcheck disable=SC1091
 
 set -o errexit
@@ -5,13 +7,12 @@ set -o pipefail
 set -o nounset
 set +o xtrace
 
-export MESSAGEBOARD_PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
-export WORKSPACE_DIR="$(cd "${MESSAGEBOARD_PROJECT_DIR}" && cd .. && pwd)" 
-export DATACENTER_PROJECT_DIR="${WORKSPACE_DIR}/datacenter-prj"
-
-echo WORKSPACE_DIR $WORKSPACE_DIR
-echo MESSAGEBOARD_PROJECT_DIR $MESSAGEBOARD_PROJECT_DIR
-echo DATACENTER_PROJECT_DIR $DATACENTER_PROJECT_DIR
+export MESSAGEBOARD_PROJECT_DIR
+MESSAGEBOARD_PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
+export WORKSPACE_DIR
+WORKSPACE_DIR="$(cd "${MESSAGEBOARD_PROJECT_DIR}" && cd .. && pwd)" 
+export DATACENTER_PROJECT_DIR
+DATACENTER_PROJECT_DIR="${WORKSPACE_DIR}/datacenter-prj"
 
 cd "${WORKSPACE_DIR}"
 
@@ -19,6 +20,12 @@ if [ ! -d "${DATACENTER_PROJECT_DIR}" ]
 then
   git clone git@github.com:maxmin13/datacenter-prj.git
 fi
+
+# override the default configuration files in datacenter-prj.
+cp "${MESSAGEBOARD_PROJECT_DIR}/provision/playbooks/variables/datacenter.json" "${DATACENTER_PROJECT_DIR}"/config
+cp "${MESSAGEBOARD_PROJECT_DIR}/provision/playbooks/variables/hostedzone.json" "${DATACENTER_PROJECT_DIR}"/config
+
+echo "Datacenter configuration files copied."
 
 cd "${DATACENTER_PROJECT_DIR}"/bin
 chmod 755 make.sh
@@ -31,7 +38,7 @@ cd "${DATACENTER_PROJECT_DIR}"/provision
 
 echo "Datacenter provisioned"
 
-PYTHONPATH="${MESSAGEBOARD_PROJECT_DIR}"/src
+export PYTHONPATH="${MESSAGEBOARD_PROJECT_DIR}"/src
 python -m venv "${MESSAGEBOARD_PROJECT_DIR}"/.venv
 source "${MESSAGEBOARD_PROJECT_DIR}"/.venv/bin/activate
 python3 -m pip install -r "${MESSAGEBOARD_PROJECT_DIR}"/requirements.txt
