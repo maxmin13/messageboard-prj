9f2762f1e364201034d4efc6e0c1d5feb6a18dfc
vagrant <vagrant@maxmin.com>
2023-10-18 15:19:12 +0100

    project refactoring

---
+++ bin/delete.sh
@@ -0,0 +1,20 @@
+# shellcheck disable=SC1091
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set +o xtrace
+
+export MESSAGEBOARD_PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
+export WORKSPACE_DIR="$(cd "${MESSAGEBOARD_PROJECT_DIR}" && cd .. && pwd)" 
+export DATACENTER_PROJECT_DIR="${WORKSPACE_DIR}/datacenter-prj"
+
+echo WORKSPACE_DIR $WORKSPACE_DIR
+echo MESSAGEBOARD_PROJECT_DIR $MESSAGEBOARD_PROJECT_DIR
+echo DATACENTER_PROJECT_DIR $DATACENTER_PROJECT_DIR
+
+cd ${DATACENTER_PROJECT_DIR}/bin
+
+chmod 755 delete.sh
+
+./delete.sh
--- bin/make.sh
+++ bin/make.sh
@@ -1,17 +1,45 @@
-
 # shellcheck disable=SC1091
-
-## python 3.11
 
 set -o errexit
 set -o pipefail
 set -o nounset
 set +o xtrace
-  
-PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
-export PYTHONPATH="${PROJECT_DIR}"/src
-python -m venv ${PROJECT_DIR}/.venv
-source "${PROJECT_DIR}"/.venv/bin/activate
-python3 -m pip install -r "${PROJECT_DIR}"/requirements.txt
 
+export MESSAGEBOARD_PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
+export WORKSPACE_DIR="$(cd "${MESSAGEBOARD_PROJECT_DIR}" && cd .. && pwd)" 
+export DATACENTER_PROJECT_DIR="${WORKSPACE_DIR}/datacenter-prj"
+
+echo WORKSPACE_DIR $WORKSPACE_DIR
+echo MESSAGEBOARD_PROJECT_DIR $MESSAGEBOARD_PROJECT_DIR
+echo DATACENTER_PROJECT_DIR $DATACENTER_PROJECT_DIR
+
+cd "${WORKSPACE_DIR}"
+
+if [ ! -d "${DATACENTER_PROJECT_DIR}" ]
+then
+  git clone git@github.com:maxmin13/datacenter-prj.git
+fi
+
+cd "${DATACENTER_PROJECT_DIR}"/bin
+chmod 755 make.sh
+./make.sh
+
+echo "Provisioning datacenter ..."
+
+cd "${DATACENTER_PROJECT_DIR}"/provision
+"${MESSAGEBOARD_PROJECT_DIR}"/bin/provision_datacenter.exp awsadmin
+
+echo "Datacenter provisioned"
+
+PYTHONPATH="${MESSAGEBOARD_PROJECT_DIR}"/src
+python -m venv "${MESSAGEBOARD_PROJECT_DIR}"/.venv
+source "${MESSAGEBOARD_PROJECT_DIR}"/.venv/bin/activate
+python3 -m pip install -r "${MESSAGEBOARD_PROJECT_DIR}"/requirements.txt
 deactivate
+
+echo "Installing web application ..."
+
+cd "${MESSAGEBOARD_PROJECT_DIR}"/provision
+"${MESSAGEBOARD_PROJECT_DIR}"/bin/provision_messageboard.exp awsadmin
+
+echo "Web application provisioned"
